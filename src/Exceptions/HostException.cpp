042c5176df396ec63424f2a871b549dcd7cbd4ea
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2023-12-26 21:44:01 +0100

    Better what()

--- src/Exceptions/HostException.cpp
+++ src/Exceptions/HostException.cpp
@@ -6,7 +6,7 @@ HostException::~HostException() {}
 
 const std::string HostException::what() {
     std::string result;
-    result.append("Failed to host server on port ");
+    result.append("Failed to host on port ");
     result.append(std::to_string(this->m_port));
     return result;
 }
