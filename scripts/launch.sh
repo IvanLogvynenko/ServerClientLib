9321d0c1a2e944df9e008f04cf3a99255d11f62f
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2023-12-27 21:01:03 +0100

    A bit of modification to provide more 'incode' error handling

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,64 @@
+if [ $# -gt 1 ]; then 
+    echo "Usage: $0 [port(opt)]"
+fi
+
+if [ $# -eq 0 ]; then 
+    echo "Default port will be used"
+else 
+    if [ $1 -lt 1024 ]; then
+        echo "Usage of port that is less then 1024 is not recommended as many services are using ports from 1-1024" && exit 1
+    else 
+        echo "Provided port is valid"
+    fi
+fi
+
+
+
+# Check if the server executable exists
+if [ ! -x "../build/server" ]; then
+    echo "Server executable not found or not executable."
+    {
+        cd ../build
+        if [ ! -x "Makefile" ]; then
+            cmake ..
+            if [ $? -ne 0 ]; then
+                echo "Error: CMake configuration failed."
+                exit 1
+            fi
+        fi
+        make server
+        if [ $? -ne 0 ]; then
+            echo "Error: Make failed. Check the build logs for details."
+            exit 1
+        fi
+    }
+fi
+
+# Check if the client executable exists
+if [ ! -x "../build/client" ]; then
+    echo "Client executable not found or not executable."
+    {
+        cd ../build
+        make client
+        if [ $? -ne 0 ]; then
+            echo "Error: Make failed. Check the build logs for details."
+            exit 1
+        fi
+    }
+fi
+
 cd ../
-build/server > logs/Server.log &
+echo "Execution start"
+
+build/server $1 > logs/Server.log &
 pid_server=$!
-build/client "127.0.0.1" "11999" > logs/Client.log & 
+
+build/client "127.0.0.1" $1 > logs/Client.log & 
 pid_client=$!
+
+echo "Awaiting execution to finish"
+
+wait $pid_server
+wait $pid_client
+
+echo "Finished"
