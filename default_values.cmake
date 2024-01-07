11a24b0d0c76e80b553712367ffd55b510889a38
Ivan Logvynenko <93791126+21whitereaper12@users.noreply.github.com>
2024-01-07 21:46:21 +0100

    a bit more of cmake

---
+++ default_values.cmake
@@ -0,0 +1,7 @@
+add_compile_definitions(DEFAULT_PORT="37373")
+
+add_compile_definitions(BUFFER_SIZE=50)
+add_compile_definitions(DEFAULT_MESSAGE="")
+
+add_compile_definitions(CONNECTION_HANDLING_TIMEOUT=500)
+add_compile_definitions(DEFAULT_TIMEOUT=500)
