7d7f456599bb4557f56f7a45a59b63989a5a5029
Ivan Logvynenko <93791126+21whitereaper12@users.noreply.github.com>
2024-01-11 13:15:19 +0100

    changing hash

--- ServerClientLib.cmake
+++ ServerClientLib.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
         ServerClientLib
         GIT_REPOSITORY https://github.com/IvanLogvynenko/ServerClientLib
-        GIT_TAG c7b22282ae2357379391a0fe86f9a665461b7df9 #refs/tags/v0.1.0
+        GIT_TAG 83fe4589dc1640687ab4580be2ad085ac01fcd2c #refs/tags/v0.1.1
 )
 FetchContent_MakeAvailable(ServerClientLib)
 target_compile_options(ServerClientLib PRIVATE -w)
