6c188cf19f71b9fee40525ce103324c2183dfeaa
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-08-12 23:15:39 +0200

    update installation script

--- ServerClientLib.cmake
+++ ServerClientLib.cmake
@@ -6,19 +6,4 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(ServerClientLib)
 target_compile_options(ServerClientLib PRIVATE -w)
-
-if (NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE DEBUG)
-endif()
-
-if (CMAKE_BUILD_TYPE==DEBUG) 
-    add_compile_definitions(LOGGING)
-    add_compile_definitions(FULL_LOGGING)
-    add_compile_definitions(ERROR)
-else()
-    if (CMAKE_BUILD_TYPE==RELEASE)
-        add_compile_definitions(LOGGING)
-    endif()
-endif()
-
 target_include_directories(ServerClientLib SYSTEM INTERFACE ${ServerClientLib_SOURCE_DIR}/include/)
