0021e845565bca45bed9301eff1206e059744014
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-08-18 00:24:43 +0200

    fixing invalid default value

--- default_values.cmake
+++ default_values.cmake
@@ -3,7 +3,7 @@ add_compile_definitions(DEFAULT_PORT=30000)
 add_compile_definitions(BUFFER_SIZE=1000)
 
 # set 0 if you don't want to stop the server
-add_compile_definitions(CONNECTION_TIMEOUT=500)
+add_compile_definitions(CONNECTION_TIMEOUT=15000)
 add_compile_definitions(MESSAGE_INCOME_TIMEOUT=500)
 
 add_compile_definitions(MAX_CONNECTION_ATTEMPTS=50)
