cafad380b78c8b4a7b4d854f0963decebe09ec0b
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2024-08-17 19:45:33 +0200

    add debugKit library

---
+++ references/debugKit.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+FetchContent_Declare(
+        debugKit
+        GIT_REPOSITORY https://github.com/IvanLogvynenko/debugKit
+        GIT_TAG main
+)
+FetchContent_MakeAvailable(debugKit)
+target_compile_options(debugKit PRIVATE -w)
+target_include_directories(debugKit SYSTEM INTERFACE ${debugKit_SOURCE_DIR}/include/)
+target_compile_definitions(debugKit PRIVATE EXECUTABLE_NAME="ServerClientLib")
